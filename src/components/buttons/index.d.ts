3d6b49f25cb76088c56aceea481459a91a853f9b
Scorti-Paul <scortip@gmail.com>
2024-08-23 06:48:27 +0000

    add additional button types

--- src/components/buttons/index.d.ts
+++ src/components/buttons/index.d.ts
@@ -7,7 +7,8 @@ export interface ButtonProps {
   path: string;
   ref?: (e, params = {}) => void;
   children?: string;
+  disabled?: boolean;
   loading?: boolean;
 }
 
-type ButtonType = "primary" | "secondary" | "link";
+type ButtonType = "primary" | "secondary" | "link" | "primary-btn" | "primary-link" |  "primary-link-large" | "secondary-btn" | "secondary-link" | "accent-link";
