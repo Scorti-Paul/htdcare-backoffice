1e780dd5ee9c69771db39321c1e83bdc39cdc9f0
Scorti-Paul <scortip@gmail.com>
2024-08-23 06:54:54 +0000

    edit button component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -4,7 +4,6 @@ import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 import DatePicker from "react-datepicker";
 import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
-
 export default function Input({
   label,
   optionalLabel,
@@ -27,7 +26,7 @@ export default function Input({
   const [inputBlur, setInputBlur] = useState(false);
 
   const handleBlur = (e: any) => {
-    if (optionalLabel === true) {
+    if (optionalLabel === false) {
       !e.target.value ? setInputBlur(true) : setInputBlur(false);
     }
   };
@@ -107,78 +106,77 @@ export default function Input({
               )}
             </div>
           </>
-        ) : (
+        ) : (       
           <>
-            <div
-              className={
-                inputLength === "small"
-                  ? "col-span-6 sm:col-span-6 lg:col-span-2 relative"
-                  : inputLength === "medium"
-                  ? "col-span-6 sm:col-span-3 relative"
-                  : inputLength === "large"
-                  ? "col-span-6 relative"
-                  : "col-span-6 sm:col-span-6 lg:col-span-2 relative"
-              }
+          <div
+            className={
+              inputLength === "small"
+                ? "col-span-6 sm:col-span-6 lg:col-span-2 relative"
+                : inputLength === "medium"
+                ? "col-span-6 sm:col-span-3 relative"
+                : inputLength === "large"
+                ? "col-span-6 relative"
+                : "col-span-6 sm:col-span-6 lg:col-span-2 relative"
+            }
+          >
+            <label
+              htmlFor={name}
+              className="block text-lg font-normal text-darkBlue"
             >
-              <label
-                htmlFor={name}
-                className="block text-sm font-medium text-gray-700"
-              >
-                {label}{" "}
-                <span className="mt-2 text-sm text-red-400">
-                  {optionalLabel}
-                </span>
-              </label>
-              <input
-                type={
-                  hasShowPassword === "disable"
-                    ? `${type}`
-                    : hasShowPassword
-                    ? "password"
-                    : `${type}`
-                }
-                name={name}
-                id={name}
-                value={value}
-                onChange={onChange}
-                onBlur={handleBlur}
-                required={required}
-                disabled={disable}
-                autoComplete={autoComplete}
-                placeholder={placeholder}
-                className={
-                  !inputBlur
-                    ? `mt-2 block w-full rounded-md border-gray-300 shadow-sm py-3 placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm`
-                    : `mt-2 block w-full  rounded-md border-red-400 shadow-sm py-3 placeholder:text-red-200 bg-red-50/40 focus:border-red-500 focus:ring-red-500 sm:text-sm`
-                }
-              />
-              {hasShowPassword === "disable" ? (
-                ""
-              ) : !hasShowPassword ? (
-                <div className="flex items-center gap-2">
-                  <EyeSlashIcon
-                    className="w-5 text-gray-400 absolute top-8 right-3 z-30 hover:cursor-pointer"
-                    onClick={handleShowHidePassword}
-                  />
-                </div>
-              ) : hasShowPassword ? (
-                <div className="flex items-center gap-2">
-                  <EyeIcon
-                    className="transition-all duration-100 ease-in w-5 text-gray-400 absolute top-10 right-3 z-30 hover:cursor-pointer"
-                    onClick={handleShowHidePassword}
-                  />
-                </div>
-              ) : (
-                ""
-              )}
-              {/* <span></span> */}
-              {inputBlur && (
-                <span className="mt-1 text-xs text-red-400 absolute ">
-                  This field is required
-                </span>
-              )}
-            </div>
-          </>
+              {label}{" "}
+              <span className="mt-2 text-lg text-red-400">{optionalLabel}</span>
+            </label>
+            <input
+              type={
+                hasShowPassword === "disable"
+                  ? `${type}`
+                  : hasShowPassword
+                  ? "password"
+                  : `${type}`
+              }
+              name={name}
+              id={name}
+              value={value}
+              onChange={onChange}
+              onBlur={handleBlur}
+              required={required}
+              disabled={disable}
+              autoComplete={autoComplete}
+              min={0}
+              placeholder={placeholder}
+              className={
+                !inputBlur
+                  ? `mt-1 block w-full rounded-md border-gray-300 shadow-sm placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-md transition-all duration-200 py-3`
+                  : `mt-1 block w-full rounded-md border-red-400 shadow-sm placeholder:text-red-200 bg-red-50/40 focus:border-red-500 focus:ring-red-500 sm:text-md transition-all duration-200 py-3`
+              }
+            />
+            {hasShowPassword === "disable" ? (
+              ""
+            ) : !hasShowPassword ? (
+              <div className="flex items-center gap-2">
+                <EyeSlashIcon
+                  className="w-5 text-gray-400 absolute top-11 right-3 z-30 hover:cursor-pointer"
+                  onClick={handleShowHidePassword}
+                />
+              </div>
+            ) : hasShowPassword ? (
+              <div className="flex items-center gap-2">
+                <EyeIcon
+                  className="transition-all duration-200 ease-in w-5 text-gray-400 absolute top-11 right-3 z-30 hover:cursor-pointer"
+                  onClick={handleShowHidePassword}
+                />
+              </div>
+            ) : (
+              ""
+            )}
+            {/* <span></span> */}
+            {inputBlur && (
+              <span className="mt-1 text-xs text-red-400 absolute transition-all duration-200">
+                This field is required
+              </span>
+            )}
+          </div>
+        </> 
         )
       ) : field === "textarea" ? (
         <>
@@ -195,7 +193,7 @@ export default function Input({
           >
             <label
               htmlFor={name}
-              className="block text-sm font-medium text-gray-700"
+              className="block text-lg font-normal text-darkBlue"
             >
               {label}{" "}
               <span className="mt-2 text-sm text-red-400">{optionalLabel}</span>
@@ -212,7 +210,7 @@ export default function Input({
                 className={
                   !inputBlur
                     ? `mt-1 block w-full rounded-md border-gray-300 shadow-sm resize-none placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm text-gray-700`
-                    : `mt-1 block w-full rounded-md border-red-400 shadow-sm resize-none placeholder:text-gray-400 focus:border-red-500 focus:ring-red-500 sm:text-sm`
+                    : `mt-1 block w-full rounded-md border-red-400 shadow-sm placeholder:text-red-200 bg-red-50/40 focus:border-red-500 focus:ring-red-500 sm:text-sm`
                 }
                 defaultValue={""}
               />
@@ -234,10 +232,10 @@ export default function Input({
           >
             <label
               htmlFor={name}
-              className="block text-sm font-medium text-gray-700"
+              className="block text-lg font-normal text-darkBlue"
             >
               {label}{" "}
-              <span className="mt-2 text-sm text-red-400">{optionalLabel}</span>
+              <span className="mt-2 text-md text-red-400">{optionalLabel}</span>
             </label>
             <select
               id={name}
@@ -246,12 +244,16 @@ export default function Input({
               onBlur={handleBlur}
               name={name}
               autoComplete={autoComplete}
-              className="mt-2 py-3 block w-full rounded-md border-gray-300 shadow-sm placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm"
+              className={
+                !inputBlur
+                  ? "mt-1 block w-full rounded-md text-gray-600 border-gray-300 shadow-sm resize-none placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm py-3"
+                  : "mt-1 block w-full rounded-md text-gray-500 border-red-400 shadow-sm resize-none placeholder:text-gray-400 focus:border-red-500 focus:ring-red-500 sm:text-sm py-3"
+              }
             >
               <>
                 {selectOptions?.map((selectOption: any) => (
                   <option value={selectOption.value} key={selectOption.value}>
-                    {selectOption.label}
+                    {selectOption.text}
                   </option>
                 ))}
               </>
@@ -271,7 +273,7 @@ export default function Input({
               className="transition-all duration-100 ease-in ring-0 mr-3 text-green-500 border-green-400 hover:ring-8  hover:ring-green-100 focus:ring-0 hover:cursor-pointer"
               name={name}
             />
-            <span className="text-gray-600 text-sm capitalize">{label}</span>
+            <span className="text-darkBlue text-sm capitalize">{label}</span>
           </label>
         </>
       ) : field === "radio" ? (
@@ -279,7 +281,7 @@ export default function Input({
           <div>
             <label
               htmlFor={id}
-              className="mb-4 mr-2 ml-3 inline-block text-sm font-medium text-gray-700 hover:cursor-pointer"
+              className="mb-4 mr-2 ml-3 inline-block text-sm font-medium text-darkBlue hover:cursor-pointer"
             >
               <input
                 type="radio"
@@ -309,11 +311,25 @@ export default function Input({
                 : "col-span-6 sm:col-span-6 lg:col-span-2"
             }
           >
-            <label className="block text-sm font-medium text-gray-700">
+            <label className="block text-sm font-medium text-darkBlue">
               {label}
             </label>
             <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
               <div className="space-y-1 text-center">
+                {/* <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                    strokeWidth={2}
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg> */}
                 <div className="flex text-sm text-gray-600">
                   <label
                     htmlFor={name}
@@ -351,7 +367,7 @@ export default function Input({
           >
             <label
               htmlFor={name}
-              className="block text-sm font-medium text-gray-700"
+              className="block text-sm font-medium text-darkBlue"
             >
               {label}
             </label>
@@ -363,7 +379,7 @@ export default function Input({
               id={name}
               autoComplete={autoComplete}
               placeholder={placeholder}
-              className="mt-1  block w-full rounded-md border-gray-300 shadow-sm placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm"
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm placeholder:text-gray-400 focus:border-green-500 focus:ring-green-500 sm:text-sm"
             />
           </div>
         </>
