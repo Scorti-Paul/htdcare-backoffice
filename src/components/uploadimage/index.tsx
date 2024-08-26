54590f00b90fedecf45959ab2611e3820fd43b8a
Scorti-Paul <scortip@gmail.com>
2024-08-26 06:53:36 +0000

    change upload input style

--- src/components/uploadimage/index.tsx
+++ src/components/uploadimage/index.tsx
@@ -2,12 +2,18 @@ import defaultImg from "assets/images/defaultImage.jpg";
 const UploadImage = ({ tempUrl, defaultImage }: any) => {
   return (
     <>
-      <div className="border rounded-md p-4 h-60 mx-auto flex justify-center">
-        <img
-          src={tempUrl || defaultImage || defaultImg}
-          alt=""
-          className="max-w-full"
-        />
+      <div className="flex items-center gap-5">
+        <div className="border rounded-full p-1 h-44 w-44 mx-auto flex justify-center">
+          <img
+            src={tempUrl || defaultImage || defaultImg}
+            alt=""
+            className="max-w-full rounded-full"
+          />
+        </div>
+        <div>
+          <h3 className="text-darkBlue text-2xl font-medium">Upload your picture</h3>
+          <p className="text-darkBlue/50 max-w-sm">For best results, use an image at least 600px by 600px in either .jpg or .png format</p>
+        </div>
       </div>
     </>
   );
