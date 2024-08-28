6a666973e6612f513916cf598e72b5d5d0475205
Scorti-Paul <scortip@gmail.com>
2024-08-28 06:51:23 +0000

    add patient medical histories endpoint

--- src/api/mutations/dentist/index.ts
+++ src/api/mutations/dentist/index.ts
@@ -1,8 +1,18 @@
-import { post, put } from "../..";
+import { get, post, put } from "../..";
 
 export const createDentist = async (body: any) => {
   return await post("dentist/create", { ...body });
 };
+
 export const updateDentist = async (body: any) => {
   return await put("dentist/update", { ...body });
+};
+
+export const getPatientMedicalHistories = async (config = {}) => {
+  try {
+    const response = await get("patient-medical-history", { ...config });
+    return response;
+  } catch (error) {
+    return error;
+  }
 };
